5b559bbd0780a781c1d71ab6d6995bac23a0d52a
droid-kk11 <153240992+droid-kk11@users.noreply.github.com>
2025-01-29 10:58:29 -0800

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -1,5 +1,15 @@
-del %USERPROFILE%\portable\2898.Scouter*
-rmdir %USERPROFILE%\portable\2898.Scouter
+@echo off
+echo Uninstalling 2898 Scouter...
+
+:: Delete the main installation folder and its contents
+del /Q "%USERPROFILE%\portable\2898.Scouter\*"
+rmdir /S /Q "%USERPROFILE%\portable\2898.Scouter"
+
+:: Remove the desktop shortcut
 del "%USERPROFILE%\Desktop\2898 Scouter.url"
-echo Done!
+
+:: Remove the uninstall shortcut from the Start Menu
+del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Uninstall 2898 Scouter.bat"
+
+echo Uninstallation complete!
 pause
